fff3f6cbe007e6c76c7d206465dce55cc0e1dc6a
MMerisalu <merilyn.merisalu.01@gmail.com>
2024-08-30 18:03:57 +0300

    input and variables execrises

---
+++ ITI0002/input-python/inputPython/main.py
@@ -0,0 +1,42 @@
+
+"""Print statements"""
+
+#print("Hello world!")
+#print("Welcome")
+#print("to school!")
+#print("""Welcome
+#to school!""")
+
+"""String endings"""
+
+#print("Hi,", end='')
+#print("Merilyn!")
+#print("Hi, ", end='')
+#print("Merilyn!")
+#print("Hi,", end=' ')
+#print("Merilyn!")
+
+"""Print strings"""
+
+#print("first", "second")
+#print(1, 2)
+#print("My age is", 31)
+#print("My age is " + str(31))
+
+"""F-strings"""
+
+#name = "Merilyn"
+#print(f"Hi {name}!")
+#price_without_tax = 20
+#print(f"Price with tax is {price_without_tax * 1.2}")
+#print("Sum: {1 + 2 + 3}")
+#print(f"Sum: {1 + 2 + 3}")
+
+"""Input"""
+
+#name = input()
+#print(name)
+
+name = input("Enter your name: ")
+print(f"Hello {name}!")
+
